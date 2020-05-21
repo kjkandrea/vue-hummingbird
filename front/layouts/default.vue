4567ffd11592e8f2fa5ff4adfe941bae75e64d17
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-05-21 16:25:49 +0900

    vuex store scaffolding

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -17,6 +17,10 @@
         </v-toolbar-items>
       </v-toolbar>
     </nav>
+
+    <div>{{name}}</div>
+    <v-btn @click="onChangeName">바이바이</v-btn>
+
     <v-row no-gutters>
       <v-col cols="12" md="4">
         <login-form></login-form>
@@ -34,6 +38,16 @@
   export default {
     components: {
       LoginForm
+    },
+    computed: {
+      name() {
+        return this.$store.state.posts.name;
+      }
+    },
+    methods: {
+      onChangeName() {
+        this.$store.commit('posts/BYE')
+      }
     }
   }
 </script>
