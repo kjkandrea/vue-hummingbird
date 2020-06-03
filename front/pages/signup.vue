ff036cff851f69bed2516308451e71457c299826
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-03 16:14:02 +0900

    라우팅 미들웨어 구성

--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -78,6 +78,20 @@
         ]
       }
     },
+    computed: {
+      me() {
+        return this.$store.state.users.me;
+      }
+    },
+    watch: {
+      me(value, oldValue) {
+        if (value) {
+          this.$router.push({
+            path: '/'
+          })
+        }
+      }
+    },
     methods: {
       onSubmitForm() {
         if(this.$refs.form.validate()){
@@ -103,7 +117,8 @@
       return {
         title: '회원가입'
       }
-    }
+    },
+    middleware: 'anonymous'
   }
 </script>
 
