bf2a3edcdc200898e9892091188327fefcbaac36
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-03 13:17:49 +0900

    더미데이터로 Post작성, PostComment 작성 구현

--- front/store/posts.js
+++ front/store/posts.js
@@ -10,6 +10,10 @@ export const mutations =  {
     const index = state.mainPosts.findIndex(v => v.id === payload.id);
     state.mainPosts.splice(index, 1);
   },
+  addComment(state, payload) {
+    const index = state.mainPosts.findIndex(v => v.id === payload.postId);
+    state.mainPosts[index].Comments.unshift(payload);
+  }
 };
 
 export const actions = {
@@ -20,5 +24,7 @@ export const actions = {
   remove({ commit }, payload) {
     commit('removeMainPost', payload)
   },
+  addComment({ commit }, payload) {
+    commit('addComment', payload)
   }
 }
