7bcc763235e921f9f5190e6d26cdcd4785d69b7a
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-04 12:06:59 +0900

    더미데이터로 무한 스크롤 구현
    const totalPosts, limit 개념은 백엔드와 연동되며 없어질 개념이나 후일을 도모하며 패스

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -27,8 +27,36 @@
       },
       mainPosts() {
         return this.$store.state.posts.mainPosts;
+      },
+      hasMorePost() {
+        return this.$store.state.posts.hasMorePost;
       }
-    }
+    },
+    // fetch : Nuxt에서 지원하는 메소드
+    // 컴포넌트가 렌더링 되기전에 Vuex에 비동기적으로 데이터를 넣을 때 사용
+    fetch({ store }) {
+      store.dispatch('posts/loadPosts')
+    },
+    mounted() {
+      window.addEventListener('scroll', this.onScroll)
+    },
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.onScroll)
+    },
+    methods: {
+      onScroll() {
+        if(
+          window.scrollY + document.documentElement.clientHeight 
+          >
+          document.documentElement.scrollHeight - 300
+        ) {
+          if (this.hasMorePost) {
+            this.$store.dispatch('posts/loadPosts')
+          }
+          //console.log('이 지점에서 포스트 로딩!')
+        }
+      }
+    },
   }
 </script>
 
--- front/store/posts.js
+++ front/store/posts.js
@@ -1,6 +1,10 @@
 export const state = () => ({
   mainPosts:[],
+  hasMorePost: true
 });
+
+const totalPosts = 51;
+const limit = 10;
 
 export const mutations =  {
   addMainPost(state, payload) {
@@ -13,6 +17,30 @@ export const mutations =  {
   addComment(state, payload) {
     const index = state.mainPosts.findIndex(v => v.id === payload.postId);
     state.mainPosts[index].Comments.unshift(payload);
+  },
+  loadPosts(state) {
+    // 남은 갯수 = 전체 갯수 - 현재 내가 불러온 갯수
+    const diff = totalPosts - state.mainPosts.length;
+    // fakePosts 더미 생성
+    const fakePosts = 
+      Array(
+        // 남은 갯수 > 10 인가? 삼항 연산
+        diff > 10 ? limit : diff
+      )
+      .fill()
+      .map(v => ({
+        id: Math.random().toString(),
+        User: {
+          id: 1,
+          nickname: '멈무',
+        },
+        content: `Hello infinite scrolling~ ${Math.random()}`,
+        Comments: [],
+        Images: []
+      }));
+      state.mainPosts = state.mainPosts.concat(fakePosts);
+      // fakePosts.length === limit 으로 불러올 갯수와 일치하는지 판단하여 불린 반환. 
+      state.hasMorePost = fakePosts.length === limit;
   }
 };
 
@@ -26,5 +54,10 @@ export const actions = {
   },
   addComment({ commit }, payload) {
     commit('addComment', payload)
+  },
+  loadPosts({ commit, state }, payload) {
+    if (state.hasMorePost) {
+      commit('loadPosts');
+    }
   }
 }
