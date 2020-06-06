939088420dbcde339d0b18c48328d6f5783dd503
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-06 11:21:12 +0900

    로그인. passport 세팅. LocalStrategy 설정

--- back/app.js
+++ back/app.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const session = require('express-session');
+const cookie = require('cookie-parser');
 
 const db = require('./models');
+const passportConfig = require('./passport');
 const app = express();
 
-// db.sequelize.sync();
-db.sequelize.sync({ force : true });
+db.sequelize.sync();
+// db.sequelize.sync({ force : true });
+passportConfig();
 
 app.use(cors( 'http://localhost:3000' ));
 app.use(express.json()); // 요청해서 온 json데이터를 파싱
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(cookie('cookiesecret'));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'cookiesecret'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.status(200).send('안녕 멈무')
@@ -24,7 +37,9 @@ app.post('/user', async (req, res, next) => {
   try {
     const hash = await bcrypt.hash(req.body.password, 12);
     const exUser = await db.User.findOne({
-      email: req.body.email
+      where: {
+        email: req.body.email
+      }
     });
 
     if (exUser) { // 이미 회원가입 되어있으면
@@ -45,6 +60,13 @@ app.post('/user', async (req, res, next) => {
     return next(err)
   }
 });
+
+const user = [
+
+]
+
+app.post('/user/login', (req, res) => {
+})
 
 app.listen(3085, () => {
   console.log(`백엔드 서버 ${3085}번 포트에서 작동중..`)
