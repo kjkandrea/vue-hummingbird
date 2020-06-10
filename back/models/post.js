8b4c246cf4f66d027a95a72516a7f3df265d3295
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-10 13:23:39 +0900

    back/models/* zerocho ch5를 덮어씌움

--- back/models/post.js
+++ back/models/post.js
@@ -13,8 +13,8 @@ module.exports = (sequelize, DataTypes) => {
     db.Post.hasMany(db.Comment);
     db.Post.hasMany(db.Image);
     db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' });
-    db.Post.belongsTo(db.Post, { as : 'Retweet' });
-    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' })
+    db.Post.belongsTo(db.Post, { as: 'Retweet' }); // RetweetId
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
   };
   return Post;
 };
